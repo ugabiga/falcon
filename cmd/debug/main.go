04fac5c5a88b148ebbeb80507a35a2cba1a4c7e4
ugabiga <ugabiga@gmail.com>
2024-01-03 11:06:37 +0900

    Add network test build

--- cmd/debug/main.go
+++ cmd/debug/main.go
@@ -1,4 +1,42 @@
 package main
 
+import (
+	"encoding/json"
+	"github.com/akrylysov/algnhsa"
+	"github.com/labstack/echo/v4"
+	"io"
+	"net/http"
+)
+
+type IPResponse struct {
+	IP string `json:"ip"`
+}
+
+func getServerIP(c echo.Context) error {
+	resp, err := http.Get("https://jsonip.io")
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, err)
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, err)
+	}
+
+	var ipResponse IPResponse
+	err = json.Unmarshal(body, &ipResponse)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, err)
+	}
+
+	return c.JSON(http.StatusOK, ipResponse)
+}
+
 func main() {
+	e := echo.New()
+
+	e.GET("/", getServerIP)
+
+	algnhsa.ListenAndServe(e, nil)
 }
