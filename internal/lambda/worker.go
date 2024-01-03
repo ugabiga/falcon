a5a69939bf35ca96c02e6da21a468d4bf253a85a
ugabiga <ugabiga@gmail.com>
2024-01-03 18:51:05 +0900

    Debug lambda worker 2

--- internal/lambda/worker.go
+++ internal/lambda/worker.go
@@ -3,16 +3,17 @@ package lambda
 import (
 	"context"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/ugabiga/falcon/internal/app"
 	"log"
 )
 
 func HandleRequest(ctx context.Context) (string, error) {
-	log.Println("HandleRequest")
-	log.Printf("ctx: %+v", ctx)
-	//a := app.InitializeApplication()
-	//if err := a.RunLambdaServer(); err != nil {
-	//	return "", err
-	//}
+	log.Printf("HandleRequest: %v", ctx)
+
+	a := app.InitializeApplication()
+	if err := a.Worker(); err != nil {
+		return "", err
+	}
 
 	return "ok", nil
 }
