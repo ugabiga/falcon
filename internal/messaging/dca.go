df54f528f37c3a5100067c163e833f0b148f04ef
ugabiga <ugabiga@gmail.com>
2024-01-07 18:01:19 +0900

    Refactor messaging

--- internal/messaging/dca.go
+++ internal/messaging/dca.go
@@ -1,13 +1,8 @@
 package messaging
 
 import (
-	"encoding/json"
 	"github.com/ThreeDotsLabs/watermill/message"
-	"github.com/ThreeDotsLabs/watermill/pubsub/gochannel"
-	"github.com/ugabiga/falcon/internal/common/debug"
 	"github.com/ugabiga/falcon/internal/service"
-	"log"
-	"time"
 )
 
 const (
@@ -25,113 +20,4 @@ type HandlerInfo struct {
 
 type DCAMessage struct {
 	TaskOrderInfo service.TaskOrderInfo
-}
-
-type DCAMessageHandler struct {
-	dcaSrv     *service.DcaService
-	sqsMessage *SQSMessage
-}
-
-func NewDCAHandler(
-	dcaSrv *service.DcaService,
-	sqsMessage *SQSMessage,
-) *DCAMessageHandler {
-	return &DCAMessageHandler{
-		dcaSrv:     dcaSrv,
-		sqsMessage: sqsMessage,
-	}
-}
-
-func (h DCAMessageHandler) Handler() HandlerInfo {
-	return HandlerInfo{
-		Name:          DCAHandlerName,
-		IncomingTopic: DCAIncomingTopic,
-		OutgoingTopic: DCAOutgoingTopic,
-		Handler:       h.Handle,
-	}
-}
-
-func (h DCAMessageHandler) Handle(msg *message.Message) ([]*message.Message, error) {
-	var newMsg DCAMessage
-	if err := json.Unmarshal(msg.Payload, &newMsg); err != nil {
-		log.Fatalf("Error occurred during unmarshalling. Err: %v", err)
-	}
-
-	log.Printf("newMsg: %+v", debug.ToJSONStr(newMsg))
-
-	if err := h.dcaSrv.Order(newMsg.TaskOrderInfo); err != nil {
-		log.Printf("Error occurred during order. Err: %v", err)
-		return nil, err
-	}
-
-	return nil, nil
-}
-
-func (h DCAMessageHandler) Watch(pubSub **gochannel.GoChannel) {
-	for {
-		log.Printf("Watching for DCA messages...")
-		messages, err := h.dcaMessages()
-		if err != nil {
-			log.Printf("Error occurred during watching. Err: %v", err)
-			return
-		}
-
-		for _, msg := range messages {
-			data, err := json.Marshal(msg)
-			if err != nil {
-				log.Printf("Error occurred during marshalling. Err: %v", err)
-				continue
-			}
-
-			if err := publish(*pubSub, DCAIncomingTopic, data); err != nil {
-				log.Printf("Error occurred during publishing. Err: %v", err)
-				continue
-			}
-			if err != nil {
-				log.Printf("Error occurred during publishing. Err: %v", err)
-			}
-		}
-
-		log.Printf("Watching for DCA messages... Done")
-		time.Sleep(30 * time.Second)
-	}
-}
-
-func (h DCAMessageHandler) WatchSQS() {
-	messages, err := h.dcaMessages()
-	if err != nil {
-		log.Printf("Error occurred during watching. Err: %v", err)
-		return
-	}
-	for _, msg := range messages {
-		if err := h.sqsMessage.Publish(msg); err != nil {
-			log.Printf("Error occurred during publishing. Err: %v", err)
-			continue
-		}
-	}
-}
-
-func (h DCAMessageHandler) HandleSQS(newMsg DCAMessage) error {
-	if err := h.dcaSrv.Order(newMsg.TaskOrderInfo); err != nil {
-		log.Printf("Error occurred during order. Err: %v", err)
-		return err
-	}
-
-	return nil
-}
-
-func (h DCAMessageHandler) dcaMessages() ([]DCAMessage, error) {
-	orders, err := h.dcaSrv.GetTarget()
-	if err != nil {
-		return nil, err
-	}
-
-	var dcaMessages []DCAMessage
-	for _, order := range orders {
-		dcaMessages = append(dcaMessages, DCAMessage{
-			TaskOrderInfo: order,
-		})
-	}
-
-	return dcaMessages, nil
 }
