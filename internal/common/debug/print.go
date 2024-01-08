c57a7376f983b214b14eba2f2b071a30be1f4f33
ugabiga <ugabiga@gmail.com>
2024-01-08 14:43:05 +0900

    Fix body dump error 2

--- internal/common/debug/print.go
+++ internal/common/debug/print.go
@@ -3,6 +3,7 @@ package debug
 import (
 	"encoding/json"
 	"log"
+	"strings"
 )
 
 func ToJSONStr(obj interface{}) string {
@@ -14,12 +15,28 @@ func ToJSONStr(obj interface{}) string {
 
 	return "\n" + string(bytes)
 }
-func ToJSONInlineStr(obj interface{}) string {
-	bytes, err := json.MarshalIndent(obj, "", "\t")
+func ToJSONInlineStr(obj []byte) string {
+	var target map[string]interface{}
+	if err := json.Unmarshal(obj, &target); err != nil {
+		log.Println(err)
+		return ""
+	}
+
+	bytes, err := json.Marshal(target)
 	if err != nil {
 		log.Println("Error marshalling JSON:", err)
 		return ""
 	}
+	formattedStr := string(bytes)
+	formattedStr = strings.ReplaceAll(formattedStr, `\n`, "")
 
-	return " " + string(bytes)
+	//log.Printf("Raw JSON: %s", inputString)
+
+	//formattedBytes, err := json.MarshalIndent(inputString, "", "\t")
+	//if err != nil {
+	//	log.Println("Error marshalling JSON:", err)
+	//	return ""
+	//}
+	//
+	return " " + formattedStr
 }
