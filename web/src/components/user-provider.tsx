479840861195d7db0a02190a6555f41310e5b26d
ugabiga <ugabiga@gmail.com>
2024-01-14 21:57:12 +0900

    Fix build issue

--- web/src/components/user-provider.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import React from "react";
-import {useQuery} from "@apollo/client";
-import {NavigationBar} from "@/components/navigation-bar";
-import {useAppDispatch} from "@/store";
-import {set} from "@/store/userSlice";
-
-
