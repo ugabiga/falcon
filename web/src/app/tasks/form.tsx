ee705f03b98ecececaa43d537e6970c2b2e2e6d3
ugabiga <ugabiga@gmail.com>
2024-01-17 15:50:27 +0900

    Add task grid params converter

--- web/src/app/tasks/form.tsx
+++ web/src/app/tasks/form.tsx
@@ -85,7 +85,7 @@ export function TaskForm(
                         </FormControl>
                         <SelectContent>
                             <SelectItem value="DCA">DCA</SelectItem>
-                            <SelectItem value="Grid" disabled>Grid</SelectItem>
+                            <SelectItem value="Grid">Grid</SelectItem>
                         </SelectContent>
                     </Select>
                     <FormMessage/>
