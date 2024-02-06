b67cc44f0b265e3a2fa77015af4327c2221dd94d
ugabiga <ugabiga@gmail.com>
2024-02-06 17:59:13 +0900

    Add localstorage to web

--- web/src/store/index.ts
+++ web/src/store/index.ts
@@ -1,21 +1,40 @@
-import {configureStore} from "@reduxjs/toolkit";
+import {combineReducers, configureStore} from "@reduxjs/toolkit";
 import {TypedUseSelectorHook, useDispatch, useSelector} from "react-redux";
 import userSlice from "@/store/userSlice";
 import tradingAccountSlice from "@/store/tradingAccountSlice";
 import taskSlice from "@/store/taskSlice";
 import refresherSlice from "@/store/refresherSlice";
+import storage from "redux-persist/lib/storage";
+import {FLUSH, PAUSE, PERSIST, persistReducer, persistStore, PURGE, REGISTER, REHYDRATE} from "redux-persist";
+import {thunk} from "redux-thunk";
 
 
-export const store = configureStore({
-    reducer: {
-        user: userSlice.reducer,
-        tradingAccount: tradingAccountSlice.reducer,
-        task: taskSlice.reducer,
-        refresher: refresherSlice.reducer,
-    }
+const rootReducer = combineReducers({
+    user: userSlice.reducer,
+    tradingAccount: tradingAccountSlice.reducer,
+    task: taskSlice.reducer,
+    refresher: refresherSlice.reducer,
 })
 
+const persistConfig = {
+    key: 'root',
+    storage: storage,
+    whitelist: []
+}
 
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+export const store = configureStore({
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
+            }
+        }).concat(thunk)
+})
+
+export const persistor = persistStore(store)
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
 export const useAppDispatch: () => AppDispatch = useDispatch
