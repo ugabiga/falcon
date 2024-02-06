ecb5ef1cc83c41fe483ca2f0c3ad80bc8d54a90e
ugabiga <ugabiga@gmail.com>
2024-02-06 17:59:13 +0900

    Add localstorage to web

--- web/src/app/providers.tsx
+++ web/src/app/providers.tsx
@@ -5,21 +5,20 @@ import {ApolloProvider} from "@apollo/client";
 import {client} from "@/graph/client";
 import {ThemeProvider} from "next-themes";
 import {Provider} from "react-redux";
-import {store} from "@/store";
+import {persistor, store} from "@/store";
 import {useSetupI18n} from "@/lib/i18n-client";
+import {PersistGate} from "redux-persist/integration/react";
 
 export default function Providers({children}: { children: React.ReactNode }) {
     const {loading} = useSetupI18n();
-
-    // if (loading) {
-    //     return <div></div>
-    // }
 
     return (
         <ThemeProvider attribute="class" defaultTheme={"system"} enableSystem>
             <ApolloProvider client={client}>
                 <Provider store={store}>
-                    {children}
+                    <PersistGate loading={null} persistor={persistor}>
+                        {children}
+                    </PersistGate>
                 </Provider>
             </ApolloProvider>
         </ThemeProvider>
