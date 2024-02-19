847ffc2d1a7dcba7e62c69f3289e0a2dcf9ba2c8
ugabiga <ugabiga@gmail.com>
2024-02-19 13:08:18 +0900

    Fix empty posthog info

--- web/src/lib/posthog-provider.tsx
+++ web/src/lib/posthog-provider.tsx
@@ -5,13 +5,11 @@ import {PostHogProvider} from 'posthog-js/react'
 import React from "react";
 
 if (typeof window !== 'undefined') {
-    if (!process.env.NEXT_PUBLIC_POSTHOG_KEY) {
-        throw new Error('NEXT_PUBLIC_POSTHOG_KEY is not defined')
+    if (process.env.NEXT_PUBLIC_POSTHOG_KEY && process.env.NEXT_PUBLIC_POSTHOG_HOST) {
+        posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
+            api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+        })
     }
-
-    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
-        api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
-    })
 }
 
 export function CSPostHogProvider({children}: { children: React.ReactNode }) {
